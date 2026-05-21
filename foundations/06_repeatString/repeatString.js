075cc8edfaefe4e84128b13c3d22a4140d1cd4d4
njambi101 <grace.nganga@student.moringaschool.com>
2026-05-22 00:47:36 +0300

    saved

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    if(num > 0){
+        let repeated = word.repeat(num);
+        console.log(repeated) 
+    }else{
+        console.log('error')
+    }
 };
 
 // Do not edit below this line
